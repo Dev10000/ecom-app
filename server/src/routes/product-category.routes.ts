a0c853cb0a2c7212702a3428437b080897715712
Angelin Calu <angelin.calu@gmail.com>
2020-10-30 15:12:59 +0200

    all backend routes good

--- server/src/routes/product-category.routes.ts
+++ server/src/routes/product-category.routes.ts
@@ -9,6 +9,6 @@ router.get('/', passport.authenticate('jwt', { session: false }), getAll);
 router.post('/', passport.authenticate('jwt', { session: false }), create);
 router.patch('/:id', passport.authenticate('jwt', { session: false }), edit);
 router.delete('/:id', passport.authenticate('jwt', { session: false }), destroy);
-router.delete('/:id/products', passport.authenticate('jwt', { session: false }), listProducts);
+router.get('/:id/products', passport.authenticate('jwt', { session: false }), listProducts);
 
 export default router;
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -8,7 +8,7 @@ const router = Router();
 router.get('/', passport.authenticate('jwt', { session: false }), getAllUsers);
 router.get('/:id', passport.authenticate('jwt', { session: false }), getUser);
 router.patch('/:id', passport.authenticate('jwt', { session: false }), editUser);
-router.get('/orders', passport.authenticate('jwt', { session: false }), getUserOrders);
+router.get('/:id/orders', passport.authenticate('jwt', { session: false }), getUserOrders);
 router.post('/', saveUser);
 
 export default router;
