bdf1b771be2dbd4ffc4589e3746d4a638e83fda9
Angelin Calu <angelin.calu@gmail.com>
2020-11-08 18:07:01 +0200

    lost all changes

--- server/src/database/QB.ts
+++ server/src/database/QB.ts
@@ -20,7 +20,7 @@ type ConditionOperator = typeof operators[number];
 type ConditionValue = string | number | boolean;
 type SortDirection = 'asc' | 'desc';
 type Constructor<T> = new () => T & IModel;
-type QueryType = 'select' | 'count' | 'delete' | 'insert';
+type QueryType = 'select' | 'count' | 'delete';
 
 interface ICondition {
     field: string;
@@ -38,27 +38,38 @@ interface IOrderBy {
     direction: SortDirection;
 }
 
-interface IQueryOptions {
-    limit: number;
-    select: string[] | '*';
-    orderBy: IOrderBy;
-    conditions: ICondition[];
-    nullConditions: INullCondition[];
-}
-
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function QB<T>(model: Constructor<T>) {
+    interface IJoin {
+        table: string;
+        localField: keyof T;
+        remoteField: string;
+    }
+
+    interface IQueryOptions {
+        table: string;
+        limit: number;
+        select: string[] | '*';
+        orderBy: IOrderBy;
+        conditions: ICondition[];
+        nullConditions: INullCondition[];
+        joins: IJoin[];
+    }
+
     class QB {
         data: IQueryOptions = {
+            table: '',
             limit: 0,
             select: '*',
             orderBy: { field: '', direction: 'asc' },
             conditions: [],
             nullConditions: [],
+            joins: [],
         };
 
         constructor(private Model: Constructor<T>) {
             this.Model = Model;
+            this.data.table = new this.Model().table;
         }
 
         /**
@@ -115,6 +126,34 @@ export default function QB<T>(model: Constructor<T>) {
             return this;
         }
 
+        // Need to filter only relevant methods to make this work!!!
+        // ...then, use something like a Proxy()
+        // Kept here for when I'll be able to fill the blanks.
+        // Until then, I'll try a join approach
+        // with(...relationships: (keyof T)[]): this {
+        //     const model: T = new this.Model();
+        //     console.log(Object.keys(model));
+        //     console.log({ relationships });
+        //     this.data.relationships = relationships.map((relationship) => model[relationship]();
+        //     return this;
+        // }
+
+        /**
+         * Ads a join part to the built query.
+         * The type of join is INNER JOIN
+         * @param table name of the table to be joined ex. 'countries'
+         * @param localField the local field ex. 'country_id'
+         * @param remoteField the remote field ex. 'id'
+         */
+        join(table: string, localField: keyof T, remoteField: string): this {
+            this.data.joins.push({
+                table,
+                localField,
+                remoteField,
+            });
+            return this;
+        }
+
         /**
          * Used for sorting the queried data. Direction can be omited. The default is 'asc'.
          * @param field string
@@ -132,13 +171,13 @@ export default function QB<T>(model: Constructor<T>) {
          * Builds up the query on the given table.
          * Default acion is 'select'.
          * @param table table where the query should run
-         * @param action [OPTIONAL] 'select' | 'count' | 'delete' | 'insert'
+         * @param action [OPTIONAL] 'select' | 'count' | 'delete'
          */
         query(table: string, action?: QueryType): QueryConfig {
             const prefix = this.buildPrefix(action || 'select');
-            const text = `${prefix} ${table} ${this.buildConditions()} ${this.buildOrderBy()}${this.buildLimit()}`;
+            const text = `${prefix} ${table} ${this.buildJoins()} ${this.buildConditions()} ${this.buildOrderBy()}${this.buildLimit()}`;
             const values = this.buildValues();
-
+            // console.log(text, values);
             return { text, values };
         }
 
@@ -146,9 +185,7 @@ export default function QB<T>(model: Constructor<T>) {
          * Get a list of resources from the database, for the given query.
          */
         async get(): Promise<T[]> {
-            const newModel: T & IModel = new this.Model();
-            const { table } = newModel;
-            const { rows } = await DB.query(this.query(table));
+            const { rows } = await DB.query(this.query(this.data.table));
             return rows;
         }
 
@@ -157,9 +194,8 @@ export default function QB<T>(model: Constructor<T>) {
          */
         async first(): Promise<T> {
             this.data.limit = 1;
-            const newModel: T & IModel = new this.Model();
-            const { table } = newModel;
-            const { rows } = await DB.query(this.query(table));
+            const newModel: T = new this.Model();
+            const { rows } = await DB.query(this.query(this.data.table));
             Object.assign(newModel, rows[0]);
             return newModel;
         }
@@ -168,39 +204,16 @@ export default function QB<T>(model: Constructor<T>) {
          * Delete a record from the database.
          */
         async delete(): Promise<void> {
-            const newModel: T & IModel = new this.Model();
-            const { table } = newModel;
-            await DB.query(this.query(table, 'delete'));
+            await DB.query(this.query(this.data.table, 'delete'));
         }
 
         /**
          * Get the number of rows from the database, for the given query.
          */
         async count(): Promise<number> {
-            const newModel: T & IModel = new this.Model();
-            const { table } = newModel;
-            const { rows } = await DB.query(this.query(table, 'count'));
+            const { rows } = await DB.query(this.query(this.data.table, 'count'));
             return rows[0].count;
         }
-
-        //  ------------ start  to ---- be ----deleted ------
-
-        // async updateOrInsert(model: T & IModel): Promise<T> {
-        //     let updateOrInsert: 'update' | 'insert';
-        //     if (model.id) {
-        //         updateOrInsert = 'update';
-        //     } else {
-        //         updateOrInsert = 'insert';
-        //     }
-
-        //     console.log('I will do an: ', updateOrInsert);
-        //     const instance = await new this.Model();
-        //     console.log({ instance });
-
-        //     return instance;
-        // }
-
-        //  ------------ end to ---- be ----deleted ------
 
         /* ---- only internal stuff below this like ---- */
 
@@ -214,8 +227,6 @@ export default function QB<T>(model: Constructor<T>) {
                     return `SELECT ${this.data.select} FROM`;
                 case 'count':
                     return `SELECT COUNT(${this.data.select}) FROM`;
-                case 'insert':
-                    return `INSERT INTO`;
                 case 'delete':
                     // TODO: if (useSoftDeletes) ... this should be turned into an update statement where deleted_at = new Date();
                     return `DELETE FROM`;
@@ -237,15 +248,17 @@ export default function QB<T>(model: Constructor<T>) {
         private buildConditions(): string {
             const conditions = this.data.conditions.reduce(
                 (acc, where, index) =>
-                    (acc += ` ${index ? 'AND' : 'WHERE'} ${where.field} ${where.operator} $${index + 1}`),
+                    (acc += ` ${index ? 'AND' : 'WHERE'} ${this.data.table}.${where.field} ${where.operator} $${
+                        index + 1
+                    }`),
                 '',
             );
 
             const nullConditions = this.data.nullConditions.reduce(
                 (acc, where, index) =>
-                    (acc += ` ${index === 0 && this.data.conditions.length === 0 ? 'WHERE' : 'AND'} ${where.field} ${
-                        where.is_null ? 'IS NULL' : 'IS NOT NULL'
-                    }`),
+                    (acc += ` ${index === 0 && this.data.conditions.length === 0 ? 'WHERE' : 'AND'} ${
+                        this.data.table
+                    }.${where.field} ${where.is_null ? 'IS NULL' : 'IS NOT NULL'}`),
                 '',
             );
 
@@ -265,13 +278,25 @@ export default function QB<T>(model: Constructor<T>) {
         private buildOrderBy(): string {
             return this.data.orderBy.field ? ` ORDER BY ${this.data.orderBy.field} ${this.data.orderBy.direction}` : '';
         }
+
+        /**
+         * Builds the JOIN [table] ON [condition] part of the query.
+         * The type of join is INNER JOIN
+         */
+        private buildJoins(): string[] {
+            return this.data.joins.map(
+                (join) =>
+                    ` INNER JOIN ${join.table} ON ${this.data.table}.${join.localField} = ${join.table}.${join.remoteField}`,
+            );
+        }
     }
 
     return new QB(model);
 }
 
 // Model.ts
-abstract class Model<T> {
+class Model<T> {
+    // abstract?
     /**
      * This is by default the primary key
      * for each model extending the Model class
@@ -282,7 +307,7 @@ abstract class Model<T> {
     /**
      * Default table name. This can be overwritten in class definition
      * By default it's the camel case pluralized version of the Model class
-     * eq. ProductCategory -> product_categories
+     * ex. ProductCategory -> product_categories
      */
     readonly table = pluralize(this.constructor.name.split(/(?=[A-Z])/).join('_')).toLowerCase();
 
@@ -293,21 +318,13 @@ abstract class Model<T> {
     readonly hidden: string[] = [];
 
     /**
-     * This includes a list of properties specific to the abstract class.
-     * Sould not be added or overwritten in the class that extends Model.
-     * TODO: find a way to prevent this (so that typescript is still happy)
+     * Saves the current state of the object into the database.g
      */
-    readonly internal: (keyof this)[] = ['table', 'hidden', 'internal'];
-
-    static instantiable() {
-        return `${this.name}`;
-    }
-
     async save() {
         let text;
 
         const { table } = this;
-        const object = removeFields(this, [...this.internal]);
+        const object = removeFields(this, ['table', 'hidden']);
         const keys = Object.keys(object);
         const values = Object.values(object);
         const valueRefs = values.map((_value, index) => `$${index + 1}`);
@@ -321,37 +338,116 @@ abstract class Model<T> {
         }
 
         const query = { text, values };
+        // console.log(query);
         return DB.query(query).then((response) => response.rows[0] as T);
     }
 
-    create(props: T) {
-        Object.assign(this, props);
-        return this;
+    // static create(props: T) {
+    //     Object.assign(this, props);
+    //     return this;
+    // }
+
+    /**
+     * Finds a record by the ID and returns a promise of an instance.
+     * @param id
+     */
+    static async find<U>(id: number): Promise<U> {
+        const instance = new this<U>();
+        const text = `SELECT * FROM ${instance.table} WHERE id=$1 LIMIT 1;`;
+        const values = [id];
+        const query = { text, values };
+        return DB.query(query).then((response) => {
+            Object.assign(instance, response.rows[0]);
+            return (instance as unknown) as U;
+        });
     }
 
     /**
      * This will return the object without the internal and hidden fields.
      */
     toJSON(): Pick<this, Exclude<keyof this, keyof this>> {
-        const removable = [...this.internal, ...this.hidden];
+        const removable = ['table', 'hidden', ...this.hidden];
         return removeFields(this, (removable as unknown) as (keyof this)[]);
+    }
+
+    /**
+     * BelongsTo Model relationship
+     * @param otherModel Other Model Name. ex. Country
+     * @param localField [OPTIONAL] ex. country_id | Defaults to the lowercase version of the other model + '_id'
+     * @param remoteField [OPTIONAL] ex. id | Defaults to 'id'
+     */
+    belongsTo<U>(otherModel: Constructor<U>, localField?: string, remoteField?: string): Promise<U> {
+        const localFieldKey = (localField as keyof this) || (`${otherModel.name.toLowerCase()}_id` as keyof this); // Country -> country_id
+        remoteField = remoteField || `id`; // default field name is 'id' unless specfied otherwise
+        const conditionValue = (this[localFieldKey] as unknown) as ConditionValue;
+        return QB<U>(otherModel).where(remoteField, conditionValue).first();
+    }
+
+    /**
+     * hasMany Model relationship
+     * @param otherModel Other Model Name. ex. User
+     * @param localField [OPTIONAL] ex. id | Defaults to 'id'
+     * @param remoteField [OPTIONAL] ex. country_id | Defaults to the lowercase version of this model + '_id'
+     */
+    hasMany<U>(otherModel: Constructor<U>, localField?: string, remoteField?: string): Promise<U[]> {
+        const localFieldKey = (localField as keyof this) || (`id` as keyof this); // default field name is 'id' unless specfied otherwise
+        remoteField = remoteField || `${this.constructor.name.toLowerCase()}_id`; // User -> users_id
+        const conditionValue = (this[localFieldKey] as unknown) as ConditionValue;
+        return QB<U>(otherModel).where(remoteField, conditionValue).get();
+    }
+}
+
+// models/Country.ts
+class Country extends Model<ICountry> {
+    async users(): Promise<IUser[]> {
+        // eslint-disable-next-line no-use-before-define
+        return this.hasMany(User);
     }
 }
 
 // models/User.ts
 class User extends Model<IUser> {
     readonly hidden = ['password'];
+
+    country(): Promise<ICountryModel> {
+        return this.belongsTo(Country);
+    }
 }
 
 // constructor usage
-const user1 = QB<IUser>(User).where('id', '>', '111').limit(5).whereNotNull('country_id').orderBy('first_name').first();
+// youtu.be/9Swrzqr4MSs?t=1647
+// req.body as {[P in keyof IUser]? : IUser[P] extends Function ? never: IUser[P]}
 
-user1.then((user) => {
-    console.log({ user });
-    user.email = 'test@email.com';
-    console.log(
-        user.save().then((updatedUser) => {
-            console.log({ updatedUser });
-        }),
-    );
-});
+// const user1 = QB<IUserModel>(User)
+//     .select('users.*', 'countries.name as country_name')
+//     .join('countries', 'country_id', 'id')
+//     .where('id', '=', '555')
+//     .first();
+
+// user1.then((user) => {
+//     console.log({ user });
+//     user.email = 'test1@email.com';
+//     console.log(
+//         user
+//             .save()
+//             .then((updatedUser) => {
+//                 console.log({ updatedUser });
+//             })
+//             .catch((err) => console.log('352', err.message)),
+//     );
+// });
+
+// User.find<IUser>(333)
+//     .then((user) => console.log(user))
+//     .catch((err) => console.log(err.message));
+
+QB<IUserModel>(User)
+    .where('id', '=', '777')
+    .first()
+    .then((user) => {
+        console.log(user.toJSON());
+        user.country()
+            .then((country) => console.log({ country }))
+            .catch((err) => console.log({ err }));
+    })
+    .catch((err) => console.log({ err }));
