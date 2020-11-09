fc34e9de933fc0c5609927e4ab53d715e1589ba1
Erkka N <erkka.nieminen@me.com>
2020-11-09 14:48:28 +0200

    Validators for login and country

---
+++ server/src/validators/login.ts
@@ -0,0 +1,19 @@
+import { body, validationResult } from 'express-validator';
+import { Request, Response, NextFunction } from 'express';
+
+export default [
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email address is a required field.')
+        .isEmail()
+        .withMessage('Given Email is not a valid email address.'),
+
+    body('password').trim().notEmpty().withMessage('Password is a required field.'),
+
+    (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
+        next();
+    },
+];
