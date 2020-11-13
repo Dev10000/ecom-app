04951661bd89786c1ba68b7c2518afa0c5773ba6
Tim <16493467+TimRNR@users.noreply.github.com>
2020-11-13 17:29:56 +0200

    WIP Implemented slugify function

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -3,13 +3,22 @@ import { Pool } from 'pg';
 import config from '.';
 
 const pool = new Pool({
-    host: config.DB.HOST,
+    host: '3.21.55.190',
     user: config.DB.USER,
-    database: config.DB.NAME,
-    password: config.DB.PASS,
+    database: 'dev_db',
+    password: 'G$oFt78$8qiZMBsbX!%!7HWVTwwU*s',
     port: Number(config.DB.PORT),
     max: 20,
 });
+
+// const pool = new Pool({
+//     host: config.DB.HOST,
+//     user: config.DB.USER,
+//     database: config.DB.NAME,
+//     password: config.DB.PASS,
+//     port: Number(config.DB.PORT),
+//     max: 20,
+// });
 
 pool.on('error', (err, _client) => {
     console.error('Unexpected error on idle client', err);
