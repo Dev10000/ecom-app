af68fe1de64e258b4cb3399cdc16188cba2988c9
Angelin Calu <angelin.calu@gmail.com>
2020-11-18 16:30:19 +0200

    bumped into error in seeder

--- server/src/database/seeders.ts
+++ server/src/database/seeders.ts
@@ -42,6 +42,7 @@ const productSpecsSeeder = async (): Promise<void> => {
 const seedData = async () => {
     console.log('\x1b[36m%s\x1b[0m', 'ℹ Started database seeding...');
 
+    // TODO: make these syncronous as they need to be ran in a specific order
     await Promise.all([
         countriesSeeder(),
         usersSeeder(),
