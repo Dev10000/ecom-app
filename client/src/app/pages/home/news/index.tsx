91e8c5f9f34e4dd8396cdf3e046c4b527c281d33
Amen Farouk <amenfarouk1@gmail.com>
2020-11-24 08:19:09 +0000

    Home (Hero2 and search Files), store feature.

--- client/src/app/pages/home/news/index.tsx
+++ client/src/app/pages/home/news/index.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
 
 const News: React.FC = (): JSX.Element => {
-    return <div>News Component start here</div>;
+    return (
+        <div className="flex max-w-screen mt-1">
+            <div className="w-full dark:bg-gray-800 text-xs flex flex-row justify-between p-16">
+                <div className="flex flex-cols-2 gap-4 w-58">
+                    <div className="flex items-left w-24">
+                        <img
+                            className="w-96 h-54"
+                            src="https://via.placeholder.com/250x150?text=Product Image"
+                            alt="Lorem ipsum"
+                        />
+                    </div>
+                    <div className="w-32">
+                        <h4 className="text-gray-300 mb-1">16.12.2020</h4>
+                        <h2 className="font-bold mb-1">Fashion Industry</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.</p>
+                    </div>
+                </div>
+                <div className="flex flex-cols-2 gap-4 w-54">
+                    <div className="flex items-left w-24">
+                        <img
+                            className="w-96 h-54"
+                            src="https://via.placeholder.com/250x150?text=Product Image"
+                            alt="Lorem ipsum"
+                        />
+                    </div>
+                    <div className="w-32">
+                        <h4 className="text-gray-300 mb-1">16.12.2020</h4>
+                        <h2 className="font-bold mb-1">Best Design tools</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.</p>
+                    </div>
+                </div>
+                <div className="flex flex-cols-2 gap-4 w-54">
+                    <div className="flex items-left w-24">
+                        <img
+                            className="w-96 h-54"
+                            src="https://via.placeholder.com/250x150?text=Product Image"
+                            alt="Lorem ipsum"
+                        />
+                    </div>
+                    <div className="w-32">
+                        <h4 className="text-gray-300 mb-1">16.12.2020</h4>
+                        <h2 className="font-bold mb-1">HR Community</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default News;
