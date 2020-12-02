a38e6f2b82345af6a754a9982e34e26e18b9aa52
Erkka N <erkka.nieminen@me.com>
2020-12-02 09:54:24 +0200

    user.controller unit tests WIP

--- server/test/unit/controllers/user.controller.ts
+++ server/test/unit/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
@@ -11,6 +12,21 @@ import server from '../../../src';
 
 chai.use(chaiHttp);
 
+const correctUser = { email: 'jpimblott0@ihg.com', password: 'secret' };
+
+// login a user to fetch a Bearer token to be used in subsequent tests
+let token: string;
+chai.request(server)
+    .post(`/api/login`)
+    .set('Content-Type', 'application/json')
+    .send(correctUser)
+    .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body.data).includes.keys(['token']);
+        token = res.body.data.token;
+    });
+
 describe('User Controller', () => {
     describe('getAllUsers', () => {
         it('Unauthenticated users should NOT be able to get all users', (done) => {
@@ -21,17 +37,51 @@ describe('User Controller', () => {
                     done();
                 });
         });
+        // TODO: Only authenticated admin users should be able to get all users
+        it('Authenticated users should? be able to get all users', (done) => {
+            chai.request(server)
+                .get(`/api/users`)
+                .set('Authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body.data).to.be.an.instanceof(Array);
+                    expect(res.body.data[0]).includes.keys(['email']);
+                    done();
+                });
+        });
     });
 
     describe('editUser', () => {
-        it('passes tests for editUser', () => {});
+        // An authenticated user should NOT be able to edit other users?
+        it('Unauthenticated users should NOT be able to edit a user', (done) => {
+            chai.request(server)
+                .patch(`/api/users/1`)
+                .set('Content-Type', 'application/json')
+                .send({ first_name: 'foo' })
+                .end((err, res) => {
+                    expect(res).to.have.status(401);
+                    done();
+                });
+        });
+        it('Authenticated users should be able to edit a user', (done) => {
+            chai.request(server)
+                .patch(`/api/users/1`)
+                .set('Authorization', `Bearer ${token}`)
+                .set('Content-Type', 'application/json')
+                .send({ first_name: 'foo' })
+                .end((err, res) => {
+                    expect(res).to.have.status(422);
+                    done();
+                });
+        });
     });
 
     describe('getUserOrders', () => {
-        it('passes tests for getUserOrders', () => {});
+        it('passes tests for getUserOrders', () => { });
     });
 
     describe('getUser', () => {
-        it('passes tests for getUser', () => {});
+        it('passes tests for getUser', () => { });
     });
 });
