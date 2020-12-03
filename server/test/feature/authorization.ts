fc3d95ac91079d20dc414fd861ea1ca398d60e19
Angelin Calu <angelin.calu@gmail.com>
2020-12-03 16:15:56 +0200

    testing access to routes WIP

--- server/test/feature/authorization.ts
+++ server/test/feature/authorization.ts
@@ -14,7 +14,7 @@ const userToken = authenticate(2, 'sboutellier1@histats.com');
 
 const checkResponseStatusTo = (
     apiEndPoint: string,
-    method: 'get' | 'post' | 'put' | 'delete',
+    method: 'get' | 'patch' | 'post' | 'put' | 'delete',
     expectedStatuses: [number, number, number], // guest, user, admin
 ) => {
     describe(`Checking access to ${method.toUpperCase()} ${apiEndPoint}:`, () => {
@@ -28,7 +28,7 @@ const checkResponseStatusTo = (
                 });
         });
 
-        it(`Users returns ${expectedStatuses[1]}`, (done) => {
+        it(`User (id=2) returns ${expectedStatuses[1]}`, (done) => {
             chai.request(server)
                 .get(apiEndPoint)
                 .set('Content-Type', 'application/json')
@@ -54,6 +54,7 @@ const checkResponseStatusTo = (
 
 describe('Testing Access to Routes', () => {
     checkResponseStatusTo('/api/users', 'get', [401, 403, 200]);
-    checkResponseStatusTo('/api/users/2', 'get', [401, 200, 200]);
-    checkResponseStatusTo('/api/users/500', 'get', [401, 403, 200]);
+    checkResponseStatusTo('/api/users/2', 'get', [401, 200, 200]); // user's own route
+    checkResponseStatusTo('/api/users/500', 'get', [401, 403, 200]); // route for a completely different user
+    checkResponseStatusTo('/api/users/2', 'patch', [401, 200, 200]); // user's own route [the 200 should be a 422 ??]
 });
