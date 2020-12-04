7ab557903f58219fd5e81a437cdeb1c87edf4f18
Angelin Calu <angelin.calu@gmail.com>
2020-12-04 12:48:15 +0200

    tests are failing

--- server/test/feature/authorization.ts
+++ server/test/feature/authorization.ts
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import chai, { expect } from 'chai';
+import chai from 'chai';
 import chaiHttp from 'chai-http';
 import 'mocha';
-import server from '../../src';
-import { authenticate, statusCode } from '../utils';
+import { authenticate, checkPatch, checkGet, checkPost, checkDelete } from '../utils';
 
 chai.use(chaiHttp);
 
@@ -13,91 +12,27 @@ const user = { id: 2, email: 'user@example.com' };
 const adminToken = authenticate(admin.id, admin.email);
 const userToken = authenticate(user.id, user.email);
 
-// console.log({ adminToken });
-
-const checkResponseStatusTo = (
-    apiEndPoint: string,
-    method: 'get' | 'patch' | 'post' | 'delete',
-    expectedStatuses: [number, number, number], // guest, user, admin
-    reqBody?: JSON,
-) => {
-    describe(`Checking access to ${method.toUpperCase()} ${apiEndPoint}:`, () => {
-        let httpRequest;
-
-        switch (method) {
-            case 'get':
-                httpRequest = chai.request(server).get(apiEndPoint).set('Content-Type', 'application/json');
-                break;
-            case 'patch':
-                httpRequest = chai
-                    .request(server)
-                    .patch(apiEndPoint)
-                    .send(reqBody)
-                    .set('Content-Type', 'application/json');
-                break;
-            case 'post':
-                httpRequest = chai
-                    .request(server)
-                    .patch(apiEndPoint)
-                    .send(reqBody)
-                    .set('Content-Type', 'application/json');
-                break;
-            case 'delete':
-                httpRequest = chai.request(server).patch(apiEndPoint).set('Content-Type', 'application/json');
-                break;
-            default:
-                throw new Error('Not Implemented');
-        }
-
-        it(`Guest returns ${expectedStatuses[0]} ${statusCode(expectedStatuses[0])}`, (done) => {
-            chai.request(server)
-                .get(apiEndPoint)
-                .set('Content-Type', 'application/json')
-                .end((err, res) => {
-                    expect(res).to.have.status(expectedStatuses[0]);
-                    done();
-                });
-        });
-
-        it(`User (id=${user.id}) returns ${expectedStatuses[1]} ${statusCode(expectedStatuses[1])}`, (done) => {
-            chai.request(server)
-                .get(apiEndPoint)
-                .set('Content-Type', 'application/json')
-                .set('Authorization', `Bearer ${userToken}`)
-                .end((err, res) => {
-                    expect(res).to.have.status(expectedStatuses[1]);
-                    done();
-                });
-        });
-
-        it(`Admin returns ${expectedStatuses[2]} ${statusCode(expectedStatuses[2])}`, (done) => {
-            chai.request(server)
-                .get(apiEndPoint)
-                .set('Content-Type', 'application/json')
-                .set('Authorization', `Bearer ${adminToken}`)
-                .end((err, res) => {
-                    expect(res).to.have.status(expectedStatuses[2]);
-                    done();
-                });
-        });
-    });
-};
+const roles = ['Guest', 'User', 'Admin'];
+const tokens = ['', userToken, adminToken];
 
 describe('Authorization Testing', () => {
     // server\src\routes\article.routes.ts
-    checkResponseStatusTo('/api/articles', 'get', [401, 403, 200]);
-    // router.post('/', Verify.isAdmin, articleValidator, create);
-    // router.get('/', getPublished);
-    // router.get('/all', Verify.isAdmin, getAll);
-    // router.get('/:id', Verify.isUser, getSingle);
-    // router.patch('/:id', Verify.isAdmin, articleValidator, edit);
-    // router.delete('/:id', Verify.isAdmin, destroy);
+    describe('Articles', () => {
+        checkGet(roles, tokens, '/api/articles', [401, 403, 200]);
+        checkPost(roles, tokens, '/api/articles', [401, 403, 200]);
+        checkGet(roles, tokens, '/api/articles/all', [401, 403, 200]);
+        checkGet(roles, tokens, '/api/articles/1', [200, 200, 200]); // should return 404
+        checkPatch(roles, tokens, '/api/articles/1', [401, 403, 200]); // should return 404
+        checkDelete(roles, tokens, '/api/articles/1', [401, 403, 200]); // should return 404
+    });
 
     // server\src\routes\user.routes.ts
-    checkResponseStatusTo('/api/users', 'get', [401, 403, 200]);
-    checkResponseStatusTo(`/api/users/${user.id}`, 'get', [401, 200, 200]); // user's own route
-    checkResponseStatusTo('/api/users/500', 'get', [401, 403, 200]); // route for a completely different user
-    checkResponseStatusTo('/api/users/2', 'patch', [401, 200, 200]); // user's own route
-    checkResponseStatusTo('/api/users/2/orders', 'get', [401, 403, 200]); // user's own route, but only admin should access
-    checkResponseStatusTo('/api/users/2/articles', 'get', [401, 403, 200]); // user's own route, but only admin should access
+    describe('Users', () => {
+        checkGet(roles, tokens, '/api/users', [401, 403, 200]);
+        checkGet(roles, tokens, `/api/users/${user.id}`, [401, 200, 200]); // user's own route
+        checkGet(roles, tokens, '/api/users/500', [401, 403, 200]); // route for a completely different user
+        checkPatch(roles, tokens, `/api/users/${user.id}`, [401, 200, 200]); // user's own route
+        checkGet(roles, tokens, '/api/users/2/orders', [401, 403, 200]); // user's own route, but only admin should access
+        checkGet(roles, tokens, '/api/users/2/articles', [401, 403, 200]); // user's own route, but only admin should access
+    });
 });
--- server/test/utils.ts
+++ server/test/utils.ts
@@ -1,5 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import jwt from 'jsonwebtoken';
+import chai, { expect } from 'chai';
 import config from '../src/config';
+
+import server from '../src';
 
 // https://www.restapitutorial.com/httpstatuscodes.html
 export const statuses = [
@@ -46,4 +50,94 @@ export const authenticate = (id: number, email: string): string => {
 export const statusCode = (code: number): string => {
     const httpStatus = statuses.find((status) => status.code === code);
     return httpStatus ? httpStatus.status : 'Unknown';
+};
+
+export const checkGet = (
+    roles: string[],
+    bearerTokens: string[],
+    apiEndPoint: string,
+    expectedStatuses: number[],
+): void => {
+    describe(`Checking HTTP responses to GET ${apiEndPoint}:`, () => {
+        roles.forEach((role, index) => {
+            it(`${role} returns ${expectedStatuses[index]} ${statusCode(expectedStatuses[index])}`, (done) => {
+                chai.request(server)
+                    .get(apiEndPoint)
+                    .set('Authorization', `Bearer ${bearerTokens[index]}`)
+                    .end((req, res) => {
+                        expect(res).to.have.status(expectedStatuses[index]);
+                        done();
+                    });
+            });
+        });
+    });
+};
+
+export const checkPost = (
+    roles: string[],
+    bearerTokens: string[],
+    apiEndPoint: string,
+    expectedStatuses: number[],
+    reqBody?: JSON,
+): void => {
+    describe(`Checking HTTP responses to POST ${apiEndPoint}:`, () => {
+        roles.forEach((role, index) => {
+            it(`${role} returns ${expectedStatuses[index]} ${statusCode(expectedStatuses[index])}`, (done) => {
+                chai.request(server)
+                    .post(apiEndPoint)
+                    .send(reqBody || {})
+                    .type('application/json')
+                    .set('Authorization', `Bearer ${bearerTokens[index]}`)
+                    .end((req, res) => {
+                        expect(res).to.have.status(expectedStatuses[index]);
+                        done();
+                    });
+            });
+        });
+    });
+};
+
+export const checkPatch = (
+    roles: string[],
+    bearerTokens: string[],
+    apiEndPoint: string,
+    expectedStatuses: number[],
+    reqBody?: JSON,
+): void => {
+    describe(`Checking HTTP responses to PATCH ${apiEndPoint}:`, () => {
+        roles.forEach((role, index) => {
+            it(`${role} returns ${expectedStatuses[index]} ${statusCode(expectedStatuses[index])}`, (done) => {
+                chai.request(server)
+                    .patch(apiEndPoint)
+                    .send(reqBody || {})
+                    .type('application/json')
+                    .set('Authorization', `Bearer ${bearerTokens[index]}`)
+                    .end((req, res) => {
+                        expect(res).to.have.status(expectedStatuses[index]);
+                        done();
+                    });
+            });
+        });
+    });
+};
+
+export const checkDelete = (
+    roles: string[],
+    bearerTokens: string[],
+    apiEndPoint: string,
+    expectedStatuses: number[],
+): void => {
+    describe(`Checking HTTP responses to DELETE ${apiEndPoint}:`, () => {
+        roles.forEach((role, index) => {
+            it(`${role} returns ${expectedStatuses[index]} ${statusCode(expectedStatuses[index])}`, (done) => {
+                chai.request(server)
+                    .patch(apiEndPoint)
+                    .set('Authorization', `Bearer ${bearerTokens[index]}`)
+                    .end((req, res) => {
+                        expect(res).to.have.status(expectedStatuses[index]);
+                        done();
+                    });
+            });
+        });
+    });
 };
