ea3026bcf7455737960c1d90a7de61639269e45a
Amen Farouk <amenfarouk1@gmail.com>
2020-12-11 11:00:56 +0200

    Last changes are set back agian

--- client/src/app/pages/main/home/featured/index.tsx
+++ client/src/app/pages/main/home/featured/index.tsx
@@ -5,70 +5,74 @@ import Img3 from './bag.png';
 
 const Featured: React.FC = (): JSX.Element => {
     return (
-        <div className="flex flex-cols-3 gap-4 max-w-full mb-16">
-            <div className="flex mx-auto px-4 sm:max-w-auto mx-auto rounded-xl shadow-md overflow-hidden">
-                <div className="md:flex">
-                    <div className="flex-1 md:flex-shrink-0 relative">
-                        <img className="h-56 w-full object-cover border-2 md:w-62" src={Img1} alt="img" />
-                        <span className="text-center text-white bg-red-500 absolute top-4 left-4 w-10">HOT</span>
-                    </div>
-                    <div className="flex-1 pl-4">
-                        <div className="uppercase tracking-wide text-sm font-semibold mt-8 mb-6">
-                            Blue Swade Nike <br /> Sneakers
-                        </div>
-                        <div>
-                            <span className="text-yellow-400">&#10025; &#10025; &#10025; &#10025;</span>
-                            <span>&#10025;</span>
-                        </div>
-                        <br />
-                        <div>
-                            <span className="text-red-400">$ 299 </span>
-                            <span className="line-through text-gray-300">$ 399</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <hr className="w-1/2 m-auto mt-2 mb-16 border-3 border-gray-300" />
 
-            <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl shadow-md overflow-hidden">
-                <div className="md:flex">
-                    <div className="flex-1 md:flex-shrink-0 relative">
-                        <img className="h-56 w-full object-cover border-2 md:w-62" src={Img2} alt="img" />
-                        <span className="text-center text-white bg-red-500 absolute top-4 left-4 w-10">HOT</span>
-                    </div>
-                    <div className="flex-1 pl-4">
-                        <div className="uppercase tracking-wide text-sm font-semibold mt-8 mb-6">
-                            Nike Air Max <br /> 200 React
+            <div className="flex flex-cols-3 gap-4 max-w-full mb-16">
+                <div className="flex mx-auto px-4 sm:max-w-auto mx-auto rounded-xl shadow-md overflow-hidden">
+                    <div className="md:flex">
+                        <div className="flex-1 md:flex-shrink-0 relative">
+                            <img className="h-56 w-full object-cover border-2 md:w-62" src={Img1} alt="img" />
+                            <span className="text-center text-white bg-red-500 absolute top-4 left-4 w-10">HOT</span>
                         </div>
-                        <div>
-                            <span className="text-yellow-400">&#10025; &#10025; &#10025; &#10025;</span>
-                            <span>&#10025;</span>
-                        </div>
-                        <br />
-                        <div>
-                            <span className="text-red-400">$ 199 </span>
-                            <span className="line-through text-gray-300">$ 299</span>
+                        <div className="flex-1 pl-4 bg-gray-100">
+                            <div className="uppercase tracking-wide text-sm font-semibold mt-8 mb-6">
+                                Blue Swade Nike <br /> Sneakers
+                            </div>
+                            <div>
+                                <span className="text-yellow-400">&#10025; &#10025; &#10025; &#10025; &nbsp;</span>
+                                <span>&#10025;</span>
+                            </div>
+                            <br />
+                            <div>
+                                <span className="text-red-400">$ 299 </span>
+                                <span className="line-through text-gray-300">$ 399</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl shadow-md overflow-hidden">
-                <div className="md:flex">
-                    <div className="flex-1 md:flex-shrink-0 relative">
-                        <img className="h-56 w-full object-cover border-2 md:w-62" src={Img3} alt="img" />
-                        <span className="text-center text-white bg-red-500 absolute top-4 left-4 w-10">HOT</span>
+
+                <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl shadow-md overflow-hidden">
+                    <div className="md:flex">
+                        <div className="flex-1 md:flex-shrink-0 relative">
+                            <img className="h-56 w-full object-cover border-2 md:w-62" src={Img2} alt="img" />
+                            <span className="text-center text-white bg-red-500 absolute top-4 left-4 w-10">HOT</span>
+                        </div>
+                        <div className="flex-1 pl-4 bg-gray-100">
+                            <div className="uppercase tracking-wide text-sm font-semibold mt-8 mb-6">
+                                Nike Air Max <br /> 200 React
+                            </div>
+                            <div>
+                                <span className="text-yellow-400">&#10025; &#10025; &#10025; &#10025; &nbsp;</span>
+                                <span>&#10025;</span>
+                            </div>
+                            <br />
+                            <div>
+                                <span className="text-red-400">$ 199 </span>
+                                <span className="line-through text-gray-300">$ 299</span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex-1 pl-4">
-                        <div className="uppercase tracking-wide text-sm font-semibold mt-8 mb-6">
-                            Nike Air Max <br /> 100 React
+                </div>
+                <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl shadow-md overflow-hidden">
+                    <div className="md:flex">
+                        <div className="flex-1 md:flex-shrink-0 relative">
+                            <img className="h-56 w-full object-cover border-2 md:w-62" src={Img3} alt="img" />
+                            <span className="text-center text-white bg-red-500 absolute top-4 left-4 w-10">HOT</span>
                         </div>
-                        <div>
-                            <span className="text-yellow-400">&#10025; &#10025; &#10025; &#10025;</span>
-                            <span>&#10025;</span>
-                        </div>
-                        <br />
-                        <div>
-                            <span className="text-red-400">$ 399 </span>
-                            <span className="line-through text-gray-300">$ 499</span>
+                        <div className="flex-1 pl-4 bg-gray-100">
+                            <div className="uppercase tracking-wide text-sm font-semibold mt-8 mb-6">
+                                Nike Air Max <br /> 100 React
+                            </div>
+                            <div>
+                                <span className="text-yellow-400">&#10025; &#10025; &#10025; &#10025; &nbsp;</span>
+                                <span>&#10025;</span>
+                            </div>
+                            <br />
+                            <div>
+                                <span className="text-red-400">$ 399 </span>
+                                <span className="line-through text-gray-300">$ 499</span>
+                            </div>
                         </div>
                     </div>
                 </div>
--- client/src/app/pages/main/home/news/index.tsx
+++ client/src/app/pages/main/home/news/index.tsx
@@ -5,80 +5,72 @@ import Img3 from './logo3.png';
 
 const News: React.FC = (): JSX.Element => {
     return (
-        <div className="flex flex-cols-3 gap-4 max-w-full mb-16">
-            <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl rounded-yl shadow-md overflow-hidden">
-                <div className="md:flex">
-                    <div className="flex-1 md:flex-shrink-0">
-                        <img
-                            className="mt-8 mb-7 h-56 w-full object-cover border-r-2 md:w-48 h-auto"
-                            src={Img1}
-                            alt="img"
-                        />
-                    </div>
-                    <div className="flex-1 pl-4">
-                        <span className="text-gray-300 text-sm font-semibold">16.12.2020</span>
-                        <div className="uppercase tracking-wide text-sm font-semibold mt-2">Fashion Industry</div>
+        <div>
+            <hr className="w-1/2 m-auto -mt-6 mb-16 border-3 border-gray-300" />
+            <div className="flex flex-cols-3 gap-4 max-w-full mb-16">
+                <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl rounded-yl shadow-md overflow-hidden">
+                    <div className="md:flex">
+                        <div className="flex-1 md:flex-shrink-0 border-2">
+                            <img className="mt-8 mb-4 h-56 w-full object-cover md:w-48 h-auto" src={Img1} alt="img" />
+                        </div>
+                        <div className="flex-1 pl-4 bg-gray-50">
+                            <span className="text-gray-300 text-sm font-semibold">16.12.2020</span>
+                            <div className="uppercase tracking-wide text-sm font-semibold mt-2">Fashion Industry</div>
 
-                        <br />
-                        <div className="-mt-4">
-                            <p>
-                                Lorem Ipsum
-                                <br /> is simply dummy
-                                <br /> text of the
-                                <br /> printing and typesetting
-                                <br /> industry.
-                            </p>
+                            <br />
+                            <div className="-mt-4">
+                                <p>
+                                    Lorem Ipsum
+                                    <br /> is simply dummy
+                                    <br /> text of the
+                                    <br /> printing and typesetting
+                                    <br /> industry.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl rounded-yl shadow-md overflow-hidden">
-                <div className="md:flex">
-                    <div className="flex-1 md:flex-shrink-0">
-                        <img className="h-56 w-full object-cover border-r-2 md:w-48 h-auto" src={Img2} alt="img" />
-                    </div>
-                    <div className="flex-1 pl-4">
-                        <span className="text-gray-300 text-sm font-semibold ">16.12.2020</span>
-                        <div className="uppercase tracking-wide text-sm font-semibold mt-2">
-                            Best Design
-                            <br /> tools
+                <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl rounded-yl shadow-md overflow-hidden">
+                    <div className="md:flex">
+                        <div className="flex-1 md:flex-shrink-0 border-2">
+                            <img className="h-56 w-full object-cover md:w-48 h-auto" src={Img2} alt="img" />
                         </div>
+                        <div className="flex-1 pl-4 bg-gray-50">
+                            <span className="text-gray-300 text-sm font-semibold ">16.12.2020</span>
+                            <div className="uppercase tracking-wide text-sm font-semibold mt-2">Best Design tools</div>
 
-                        <br />
-                        <div className="-mt-4">
-                            <p>
-                                Lorem Ipsum
-                                <br /> is simply dummy
-                                <br /> text of the
-                                <br /> printing and typesetting
-                                <br /> industry.
-                            </p>
+                            <br />
+                            <div className="-mt-4">
+                                <p>
+                                    Lorem Ipsum
+                                    <br /> is simply dummy
+                                    <br /> text of the
+                                    <br /> printing and typesetting
+                                    <br /> industry.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl rounded-yl shadow-md overflow-hidden">
-                <div className="md:flex">
-                    <div className="flex-1 md:flex-shrink-0">
-                        <img
-                            className="mt-4 mb-4 h-56 w-full object-cover border-r-2 md:w-48 h-auto"
-                            src={Img3}
-                            alt="img"
-                        />
-                    </div>
-                    <div className="flex-1 pl-4">
-                        <span className="text-gray-300 text-sm font-semibold ">16.12.2020</span>
-                        <div className="uppercase tracking-wide text-sm font-semibold mt-2">HR Community</div>
+                <div className="flex mx-auto px-4 sm:max-w-md mx-auto rounded-xl rounded-yl shadow-md overflow-hidden">
+                    <div className="md:flex">
+                        <div className="flex-1 md:flex-shrink-0 border-2">
+                            <img className="mt-4 mb-2 h-56 w-full object-cover md:w-48 h-auto" src={Img3} alt="img" />
+                        </div>
+                        <div className="flex-1 pl-4 bg-gray-50">
+                            <span className="text-gray-300 text-sm font-semibold ">16.12.2020</span>
+                            <div className="uppercase tracking-wide text-sm font-semibold mt-2">HR Community</div>
 
-                        <br />
-                        <div className="-mt-4">
-                            <p>
-                                Lorem Ipsum
-                                <br /> is simply dummy
-                                <br /> text of the
-                                <br /> printing and typesetting
-                                <br /> industry.
-                            </p>
+                            <br />
+                            <div className="-mt-4">
+                                <p>
+                                    Lorem Ipsum
+                                    <br /> is simply dummy
+                                    <br /> text of the
+                                    <br /> printing and typesetting
+                                    <br /> industry.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
