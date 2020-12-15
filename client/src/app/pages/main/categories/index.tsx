79a2d8962d026e2685fb9b8249491fb7e82ae8b3
Angelin Calu <angelin.calu@gmail.com>
2020-12-15 17:30:32 +0200

    displaying categories

--- client/src/app/pages/main/categories/index.tsx
+++ client/src/app/pages/main/categories/index.tsx
@@ -1,34 +1,38 @@
 /* eslint-disable camelcase */
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import topCategories from '../../../../utils/top_categories.json';
 import allCategories from '../../../../utils/all_categories.json';
 
 function categoryTree(parentId: number, level = 0): JSX.Element {
-    return (
-        <ul>
-            {allCategories
-                .filter((currentCategory) => currentCategory.parent_id === parentId)
-                .map((subcategory) => {
-                    return (
-                        <>
-                            <li key={subcategory.id}>{subcategory.title}</li>
-                            <li className="inline-flex">
-                                <span>{'-'.repeat(level)}</span> {categoryTree(subcategory.id, (level += 1))}
-                            </li>
-                        </>
-                    );
-                })}
+    const subcategories = allCategories.filter((currentCategory) => currentCategory.parent_id === parentId);
+
+    return subcategories.length ? (
+        <ul className="ml-4">
+            {subcategories.map((subcategory) => {
+                return (
+                    <>
+                        <li key={subcategory.id} className="hover:underline">
+                            <NavLink to={`categories/${subcategory.slug}`}>{subcategory.title}</NavLink>
+                        </li>
+                        {categoryTree(subcategory.id, (level += 1))}
+                    </>
+                );
+            })}
         </ul>
+    ) : (
+        <></>
     );
 }
 
 const Categories: React.FC = (): JSX.Element => {
     return (
-        <div>
+        <div className="ml-8">
+            <h1 className="text-2xl my-8">All Categories</h1>
             {topCategories.map((topCategory) => (
                 <ul key={topCategory.id}>
-                    <li className="font-bold">{topCategory.title}</li>
+                    <li className="font-bold mt-4">{topCategory.title}</li>
                     <li className="inline-flex">{categoryTree(topCategory.id)}</li>
                 </ul>
             ))}
